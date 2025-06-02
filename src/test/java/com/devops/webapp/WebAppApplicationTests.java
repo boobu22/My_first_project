8eb0677ab57d61e85c2624d9e6fc84c67bb3e769
BOOBALAN A <136874985+boobu22@users.noreply.github.com>
2025-06-02 22:43:13 +0530

    Update WebAppApplicationTests.java

--- src/test/java/com/devops/webapp/WebAppApplicationTests.java
+++ src/test/java/com/devops/webapp/WebAppApplicationTests.java
@@ -1,1 +1,15 @@
+package com.devops.webapp;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+@SpringBootTest
+@SpringJUnitConfig
+class WebAppApplicationTests {
+    
+    @Test
+    void contextLoads() {
+        // Test that the application context loads successfully
+    }
+}
